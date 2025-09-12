b068c1bb04ae2373c964f8ddf186fa31972dea19
Anton Lysenkov <soul4code@gmail.com>
2025-09-12 12:19:05 +0500

    Remov YM

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,6 @@ import { Details } from "@components";
 import { verifySession } from "@/server/auth";
 import { AuthContextProvider } from "@/providers/auth";
 import Script from "next/script";
-import YandexMetrika from "@components/YandexMetrika/YandexMerika";
 
 export const metadata = {
   title:
@@ -37,7 +36,6 @@ const RootLayout = async ({ children }) => {
                 <Header isMobile={isMobile} />
                 <Details version={1} />
                 {children}
-                <YandexMetrika />
                 <Footer />
               </StoreProvider>
             </div>
--- src/utils/ym/utilYMRegistration.js
+++ src/utils/ym/utilYMRegistration.js
@@ -1,9 +1,9 @@
-import ym from "react-yandex-metrika";
 /**
  * Утилита для яндекс метрики для отслеживания регистрации
  * @returns {*}
  */
-export const utilYMRegistration = () => ym("reachGoal", "REGISTRATION");
+export const utilYMRegistration = () =>
+  ym(78100309, "reachGoal", "REGISTRATION");
 
 /**
  * Утилита для яндекс метрики для отслеживания успешно завершенных заказов
@@ -11,7 +11,7 @@ export const utilYMRegistration = () => ym("reachGoal", "REGISTRATION");
  * @returns {*}
  */
 export const utilYMSuccessfulOrderCreation = (orderPrice) =>
-  ym("reachGoal", "SUCCESSFUL_ORDER_CREATION", {
+  ym(78100309, "reachGoal", "SUCCESSFUL_ORDER_CREATION", {
     order_price: `${orderPrice}`,
     currency: "RUB",
   });
